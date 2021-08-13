b881926721a185463d22ea93fc3512d8caca1f1e
waqasislam <clocksquiltobligetenseobtain612@protonmail.com>
2021-08-13 12:29:00 +0500

    Deleted tests

--- examples/test.py
+++
@@ -1,8 +0,0 @@
-from betterpython.Eye import modify
-@modify(int)
-def __add__(self, other):
-    if isinstance(other,str):
-        return self + int(other)
-    else:
-        return - (-other)
-print(10 + '1')
